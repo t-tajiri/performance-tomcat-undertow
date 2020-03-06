2589eb2ca6a51a77ad37cc5bf4c3cc3f75397b6c
t-tajiri <swallow.ava@gmail.com>
2020-03-06 17:00:05 +0900

    add tomcat project

---
+++ performance-tomcat/src/main/java/ttajiri/performance/tomcat/PerformanceTomcatApplication.java
@@ -0,0 +1,13 @@
+package ttajiri.performance.tomcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformanceTomcatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerformanceTomcatApplication.class, args);
+	}
+
+}
